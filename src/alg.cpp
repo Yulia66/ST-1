012534c6567e5d417852c9620589944b05ee7944
Yulia66 <ajlakejdz@gmail.com>
2026-03-01 00:43:43 +0300

    first commit

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,24 +1,65 @@
-// Copyright 2025 UNN-CS
+// Copyright 2025 UNN-IASR
 #include <cstdint>
+#include <vector>
 #include "alg.h"
 
-
 bool checkPrime(uint64_t value) {
-  // вставьте код функции
+  if (value < 2) {
+    return false;
+  }
+  if (value == 2 || value == 3) {
+    return true;
+  }
+  for (uint64_t i = 2; i * i <= value; i++) {
+    if ((value % i) == 0) {
+      return false;
+    }
+  }
   return true;
 }
 
 uint64_t nPrime(uint64_t n) {
-  // вставьте код функции
-  return 2;
+  if (n == 0) {
+    return 0;
+  }
+  uint64_t position = 0;
+  uint64_t desired = 1;
+  while (position < n) {
+    desired++;
+    if (checkPrime(desired))
+      position++;
+  }
+  return desired;
 }
 
 uint64_t nextPrime(uint64_t value) {
-  // вставьте код функции
-  return 2;
+  if (value == UINT64_MAX) {
+    return 0;
+  }
+  uint64_t next = value + 1;
+  while (!checkPrime(next)) next++;
+  return next;
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-  // вставьте код функции
-  return 2;
+  if (hbound <= 2) {
+    return 0;
+  }
+  std::vector<bool> is_prime(hbound, true);
+  is_prime[0] = false;
+  is_prime[1] = false;
+  for (uint64_t i = 2; i * i < hbound; i++) {
+    if (is_prime[i]) {
+      for (uint64_t j = i * i; j < hbound; j += i) {
+        is_prime[j] = false;
+      }
+    }
+  }
+  uint64_t sum = 0;
+  for (uint64_t i = 0; i < hbound; i++) {
+    if (is_prime[i]) {
+      sum += i;
+    }
+  }
+  return sum;
 }
--- test/tests.cpp
+++ test/tests.cpp
@@ -1,17 +1,66 @@
-// Copyright 2025 UNN-CS
-
+// Copyright 2025 UNN-IASR
 #include <gtest/gtest.h>
 #include <cstdint>
 #include "alg.h"
-
 
 TEST(st1, sumPrime1) {
   uint64_t res = sumPrime(2000000);
   uint64_t expected = 142913828922;
   EXPECT_EQ(expected, res);
 }
-TEST(st1, sumPrime2) {
-  uint64_t res = sumPrime(10);
-  uint64_t expected = 17;
-  EXPECT_EQ(expected, res);
+
+TEST(CheckPrimeTest, SmallNonPrimes) {
+  EXPECT_FALSE(checkPrime(0));
+  EXPECT_FALSE(checkPrime(1));
+}
+
+TEST(CheckPrimeTest, SmallPrimes) {
+  EXPECT_TRUE(checkPrime(2));
+  EXPECT_TRUE(checkPrime(3));
+  EXPECT_TRUE(checkPrime(5));
+  EXPECT_TRUE(checkPrime(7));
+}
+
+TEST(CheckPrimeTest, CompositeNumbers) {
+  EXPECT_FALSE(checkPrime(4));
+  EXPECT_FALSE(checkPrime(9));
+  EXPECT_FALSE(checkPrime(15));
+  EXPECT_FALSE(checkPrime(25));
+}
+
+TEST(NPrimeTest, FirstPrimes) {
+  EXPECT_EQ(nPrime(1), 2);
+  EXPECT_EQ(nPrime(2), 3);
+  EXPECT_EQ(nPrime(3), 5);
+  EXPECT_EQ(nPrime(4), 7);
+  EXPECT_EQ(nPrime(5), 11);
+}
+
+TEST(NPrimeTest, ZeroIndex) {
+  EXPECT_EQ(nPrime(0), 0);
+}
+
+TEST(NextPrimeTest, NextAfterSmallNumbers) {
+  EXPECT_EQ(nextPrime(1), 2);
+  EXPECT_EQ(nextPrime(2), 3);
+  EXPECT_EQ(nextPrime(4), 5);
+  EXPECT_EQ(nextPrime(10), 11);
+}
+
+TEST(NextPrimeTest, NextAfterPrimes) {
+  EXPECT_EQ(nextPrime(3), 5);
+  EXPECT_EQ(nextPrime(7), 11);
+  EXPECT_EQ(nextPrime(13), 17);
+}
+
+TEST(SumPrimeTest, SumBelowSmallBounds) {
+  EXPECT_EQ(sumPrime(2), 0);
+  EXPECT_EQ(sumPrime(3), 2);
+  EXPECT_EQ(sumPrime(6), 10);
+}
+
+TEST(SumPrimeTest, SumBelowLargerBounds) {
+  EXPECT_EQ(sumPrime(11), 17);
+  EXPECT_EQ(sumPrime(12), 28);
+  EXPECT_EQ(sumPrime(20), 77);
 }
